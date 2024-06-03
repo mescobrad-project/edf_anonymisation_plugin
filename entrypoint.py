da855d0eed74215ee2556a0ed32c9588de974761
Saska Cvoro <saska.cvoro@eng.it>
2024-06-03 23:08:05 +0200

    Support MRN parameter

--- entrypoint.py
+++ entrypoint.py
@@ -17,10 +17,12 @@ class GenericPlugin(EmptyPlugin):
         # Return the results
         return rows
 
-    def transform_input_data(self, data, source_name, workspace_id):
+    def transform_input_data(self, data, source_name, workspace_id, MRN):
         """Transform input data into table suitable for creating query"""
 
         data = data.reset_index(drop=True)
+        if MRN is not None:
+            data["MRN"] = MRN
 
         # Add rowid column representing id of the row in the file
         data["rowid"] = data.index + 1
@@ -309,7 +311,8 @@ class GenericPlugin(EmptyPlugin):
 
                     # Transform data in suitable form for updating trino table
                     data_transformed = self.transform_input_data(
-                        data, source_name, input_meta.data_info["workspace_id"])
+                        data, source_name, input_meta.data_info["workspace_id"],
+                        input_meta.data_info["MRN"])
                     self.upload_data_on_trino(schema_name, table_name, data_transformed,
                                               conn)
 
